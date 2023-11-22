b832a3db48089b46d3ea731befe1c66b1ada0c84
Rohith-Manjunath <rohithmanjunath20@gmail.com>
2023-11-22 17:08:11 +0530

    feat: Implement complete shopping cart functionality
    - Add ability to add items to the cart
    - Implement functionality to remove items from the cart
    - Introduce quantity management for items in the cart (increase and decrease)
    - Implement proper updates to the total price in the cart
    - Enhance user interface to reflect changes in real-time

--- Redux-Toolkit/my-app/src/pages/Home.jsx
+++ Redux-Toolkit/my-app/src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart } from "../Redux/Slice";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
+  const cart = useSelector((state) => state.cart); // Update this line
+  const dispatch = useDispatch();
+  console.log(cart);
 
   useEffect(() => {
     getAllProducts();
@@ -20,6 +25,10 @@ const Home = () => {
     return description;
   };
 
+  const Cart = (id) => {
+    dispatch(addToCart(id));
+  };
+
   return (
     <main className="w-[90%] p-5  mx-auto mt-[2rem] flex flex-wrap gap-5 items-start justify-center">
       {products.map((product) => {
@@ -28,11 +37,7 @@ const Home = () => {
             key={product.id}
             className="w-[15rem] h-auto p-4 border border-grey rounded-md flex flex-col items-start justify-between gap-2"
           >
-            <img
-              src={product.image}
-              alt=""
-              className="w-full h-[13rem] object-cover mb-4"
-            />
+            <img src={product.image} alt="" className="w-full h-[14rem] " />
             <h2 className="text-xl font-semibold text-slate-700">
               {product.title}
             </h2>
@@ -44,7 +49,10 @@ const Home = () => {
               In Stock - {product.rating.count}
             </span>
             <h4 className="text-blue-500"> Rating - {product.rating.rate}</h4>
-            <button className="bg-purple-600 text-white font-bold w-[70%] p-2 rounded-md shadow-lg active:translate-x-1 active:translate-y-1 transition-all duration-200">
+            <button
+              className="bg-purple-600 text-white font-bold w-[70%] p-2 rounded-md shadow-lg active:translate-x-1 active:translate-y-1 transition-all duration-200"
+              onClick={() => Cart(product)}
+            >
               Add to cart
             </button>
           </div>
